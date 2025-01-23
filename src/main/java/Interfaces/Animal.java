321694cbd781a996bebb3de01ea48d6dadbaa5f2
JaniHakalaYH <Jani.Hakala@yh.nackademin.se>
2025-01-23 15:44:44 +0100

    some more interfaces

--- src/main/java/Interfaces/Animal.java
+++ src/main/java/Interfaces/Animal.java
@@ -1,6 +1,10 @@
 package Interfaces;
 
 interface FlightEnabled {
+
+    double MILES_TO_KM = 1.60934;
+    double KM_TO_MILES = 0.621371;
+    void move();
     void takeOff();
     void land();
     void fly();
--- src/main/java/Interfaces/Main.java
+++ src/main/java/Interfaces/Main.java
@@ -15,5 +15,25 @@ public class Main {
         flier.fly();
         tracked.track();
         flier.land();
+
+        inFlight(flier);
+        inFlight(new Jet());
+        Trackable truck = new Truck();
+        truck.track();
+
+        double kmsTraveled = 100;
+        double milesTraveled = kmsTraveled * FlightEnabled.KM_TO_MILES;
+        System.out.printf("The truck traveled %.2f km or %.2f miles%n", kmsTraveled, milesTraveled);
+
     }
+
+    public static void inFlight(FlightEnabled flier){
+        flier.takeOff();
+        flier.fly();
+        if(flier instanceof Trackable tracked){
+            tracked.track();
+        }
+        flier.land();
+    }
+
 }
